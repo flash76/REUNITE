5101bcfe1d9275d8996f34089075e0dd6d0847f9
flash76 <30060599+flash76@users.noreply.github.com>
2018-12-17 09:09:37 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+if [[ $OSTYPE == "darwin" ]]; then
+  echo "Good child. You are on a Mac"
+  if xcode-select --install 2>&1 | grep installed; then
+    echo "Even better child. You have Xcode. Building Xcode Projects..."
+    {
+      xcodebuild -project mobileios/REUNITE.xcodeproj -alltargets -configuration Release
+    }
+    echo "Done."
+    echo "Building Android projects..."
+    {
+      cd kiosk/
+      ./gradlew 
+  else
+    echo "You need the Xcode to build the project. Exiting..."
+    exit 1
+  fi
+else
+  echo "You need a Mac to build the project. Exiting..."
+  exit 1
+fi
